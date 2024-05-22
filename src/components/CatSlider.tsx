d057747647fbb77683ce53baacdd6cb1002cf341
Vic Istomin <vic.istomin@gmail.com>
2024-05-23 02:51:57 +0400

    Update handling of the 'disabled' state for UI elements during loading or after error

--- src/components/CatSlider.tsx
+++ src/components/CatSlider.tsx
@@ -1,19 +1,23 @@
 import SliderContent from '@/components/SliderContent.tsx';
 import SliderDots from '@/components/SliderDots.tsx';
 import { useGetCatsQuery } from '@/services/catsApi.ts';
+import { EmptyHandler } from '@/types';
 import { FC, useState } from 'react';
 
 const CatSlider: FC = () => {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
 
-  const { data, refetch } = useGetCatsQuery(10);
+  const { data, refetch, error, isLoading, isFetching } = useGetCatsQuery(10);
 
-  const handlePrev = () => {
+  const isEmpty: boolean =
+    !!error || isLoading || isFetching || !data || data?.length === 0;
+
+  const handlePrev: EmptyHandler = () => {
     setCurrentIndex((prev) =>
       prev === 0 ? (data?.length ?? 1) - 1 : prev - 1
     );
   };
-  const handleNext = () => {
+  const handleNext: EmptyHandler = () => {
     setCurrentIndex((prev) =>
       prev === (data?.length ?? 1) - 1 ? 0 : prev + 1
     );
@@ -25,7 +29,11 @@ const CatSlider: FC = () => {
         Get new images
       </button>
       <div className="slider-wrapper">
-        <button onClick={() => handlePrev()} className="left-button">
+        <button
+          disabled={isEmpty}
+          onClick={() => handlePrev()}
+          className="left-button"
+        >
           <img src="/chevron.svg" alt="previous image" width={40} height={40} />
         </button>
         <div className="slider-content">
@@ -35,11 +43,17 @@ const CatSlider: FC = () => {
             onNextImage={handleNext}
           />
         </div>
-        <button onClick={() => handleNext()} className="right-button">
+        <button
+          disabled={isEmpty}
+          onClick={() => handleNext()}
+          className="right-button"
+        >
           <img src="/chevron.svg" alt="next image" width={40} height={40} />
         </button>
       </div>
-      <SliderDots currentIndex={currentIndex} count={data?.length ?? 1} />
+      {!isEmpty && (
+        <SliderDots currentIndex={currentIndex} count={data?.length ?? 1} />
+      )}
     </div>
   );
 };
